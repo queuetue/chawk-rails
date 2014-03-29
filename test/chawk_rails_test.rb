7103f91700c96c6e356a8ab05c6b7fecc0cb2455
queuetue <scott@cootermuck.com>
2014-03-29 17:59:48 -0400

    Moved to 0.3.0

--- test/chawk_rails_test.rb
+++ test/chawk_rails_test.rb
@@ -5,7 +5,7 @@ class ChawkRailsTest < ActiveSupport::TestCase
   def setup
     Chawk.clear_all_data!
     @agent =  Chawk::Models::Agent.first || Chawk::Models::Agent.create(:name=>"Test User")
-    @addr = Chawk.addr(@agent,'a:b')
+    @node = Chawk.node(@agent,'a:b')
   end
 
   test "truth" do
@@ -14,14 +14,14 @@ class ChawkRailsTest < ActiveSupport::TestCase
 
   test "has a good agent" do
   	assert_raises(ArgumentError) do 
-    	@addr = Chawk.addr(nil,'a:b')
-    	@addr = Chawk.addr(Object.new,'a:b')
-    	@addr = Chawk.addr(Chawk::Models::Agent,'a:b')
+    	@node = Chawk.node(nil,'a:b')
+    	@node = Chawk.node(Object.new,'a:b')
+    	@node = Chawk.node(Chawk::Models::Agent,'a:b')
   	end
   end
 
   test "has key" do
-  	assert_respond_to @addr,:key
+  	assert_respond_to @node,:key
   end
 
 end
